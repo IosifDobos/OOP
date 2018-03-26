86d181a9ae57303de7a8499f07b916b0daa235d7
IosifDobos <c16735789@mydit.ie>
2018-03-26 12:27:53 +0100

    pull JoeGame to my repository

---
+++ Assignment II/JoeGame/Assets/Scripts/AttackScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackScript : MonoBehaviour {
+    int hitpoints = 0;
+    float toTargert;
+    float range = 5;
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	/// <summary>
+    /// 
+    /// </summary>
+    void Update () {
+        if (Input.GetButtonDown("Hit"))
+        {
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward),out hit))
+            {
+                toTargert = hit.distance;
+                if (toTargert < range)
+                {
+                    hit.transform.SendMessage("DeductPoints", hitpoints, SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+    }
+}
